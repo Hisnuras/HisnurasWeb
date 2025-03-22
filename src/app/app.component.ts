b88efb0ebd0df4aa1388420a42b2310c260ad36c
Hisnuras <benjamin.macquet@outlook.fr>
2025-03-22 15:03:23 +0100

    Create Home component and update navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
-import { MatButtonModule } from '@angular/material/button';
 
 import { NavbarComponent } from './components/navbar/navbar.component';
 
@@ -9,7 +8,11 @@ import { NavbarComponent } from './components/navbar/navbar.component';
 @Component({
   selector: 'app-root',
   standalone: true, // Déclarer le composant comme autonome
-  imports: [RouterOutlet , MatButtonModule , NavbarComponent],
+  imports: [
+    RouterOutlet,
+    NavbarComponent
+
+  ], // Ajouter les modules nécessaires
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,16 +1,30 @@
-import { Component } from '@angular/core';
+import { Component , HostListener  } from '@angular/core';
 
 
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
 
 @Component({
   selector: 'app-navbar',
   standalone: true, // Indique que c'est un composant autonome
-  imports: [MatToolbarModule, MatButtonModule], // Ajoute les modules nécessaires
+  imports: [
+    MatToolbarModule,
+     MatButtonModule,
+     MatIconModule,
+    ], // Ajoute les modules nécessaires
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
-
+  // Détecte le scroll et applique ou retire la classe 'scrolled' sur la navbar
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    const navbar = document.querySelector('.mat-toolbar');
+    if (window.scrollY > 50) {
+      navbar?.classList.add('scrolled');
+    } else {
+      navbar?.classList.remove('scrolled');
+    }
+  }
 }
